8251bf126743ef78e6e7795e9b0d295374531b85
MrRaghavan <raghavofbappa@gmail.com>
2022-11-20 12:46:18 +0530

    While Loop example

---
+++ Assignments/While_Loop.py
@@ -0,0 +1,11 @@
+# Code that gives following as an output
+# iNeuroniNeuroniNeuroniNeuron
+
+string = "iNeuron";
+counter = 0;
+
+# By default end line character is added at the end of print()
+# Here we are replacing the end line character with empty string
+while(counter < 4):
+    print(string, end="");
+    counter = counter + 1;
