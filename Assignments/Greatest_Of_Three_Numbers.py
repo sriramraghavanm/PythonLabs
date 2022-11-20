e5ca923014c47cf87bd47cb5b979ee3b99825a64
MrRaghavan <raghavofbappa@gmail.com>
2022-11-20 14:59:55 +0530

    Greatest of three numbers

--- Assignments/Greatest_Of_Three_Numbers.py
+++ Assignments/Greatest_Of_Three_Numbers.py
@@ -1,14 +1,14 @@
 # Code to take 3 numbers as an input from the user and display the greatest no as output.
 
 # These statements will accept the input from user and also typecast it into integer
-no1 = int(input("Enter the first number - "));
-no2 = int(input("Enter the second number - "));
-no3 = int(input("Enter the third number - "));
+number1 = int(input("Enter the first number - "));
+number2 = int(input("Enter the second number - "));
+number3 = int(input("Enter the third number - "));
 
 # Logic to check greatest of three numbers
-if((no1 > no2) & (no1 > no3)):
-    print("Greatest number is - ", no1);
-elif((no2 > no3) & (no2 > no1)):
-    print("Greatest number is - ", no2);
+if((number1 > number2) & (number1 > number3)):
+    print("Greatest number is - ", number1);
+elif((number2 > number3) & (number2 > number1)):
+    print("Greatest number is - ", number2);
 else:
-    print("Greatest number is - ", no3);
+    print("Greatest number is - ", number3);
