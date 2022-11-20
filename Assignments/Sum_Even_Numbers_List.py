3d0cf20f9c712ff033fbe5c6e4a909ae8814e740
MrRaghavan <raghavofbappa@gmail.com>
2022-11-20 15:28:26 +0530

    Sum of even numbers from a list

---
+++ Assignments/Sum_Even_Numbers_List.py
@@ -0,0 +1,14 @@
+# Code that displays the sum of all the even numbers from the given list
+
+numberList = [12, 75, 150, 180, 145, 525, 50];
+sumOfEvenNumbers = 0;
+
+# for loop to traverse the list
+# for i in numberList:
+#    print(i);
+
+for number in numberList:
+    if(number % 2 == 0):
+        sumOfEvenNumbers = sumOfEvenNumbers + number;
+
+print("Sum of even numbers from the list is - ", sumOfEvenNumbers);
