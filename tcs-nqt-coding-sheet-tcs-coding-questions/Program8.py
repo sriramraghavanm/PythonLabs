0daa74353b762740f1af21197c0503edea7eb437
sriramraghavanm <raghavan.23@live.in>
2022-11-28 23:31:44 +0530

    Made changes to syntax

--- tcs-nqt-coding-sheet-tcs-coding-questions/Program8.py
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program8.py
@@ -1,9 +1,9 @@
 # Check whether a number is positive or negative
 # Problem statement: Given a number n check whether it’s positive or negative.
 
-no = input("Enter a number - ");
+no = input("Enter a number - ")
 
-if(int(no) >= 0):
-    print("Number is positive")
+if int(no) >= 0:
+    print("Number is positive.")
 else:
-    print("Numbe is negative");
+    print("Number is negative.")
