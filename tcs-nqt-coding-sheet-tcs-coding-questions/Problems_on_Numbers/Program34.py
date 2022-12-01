8df45ad7bf35092715e9197f961a0238ee372754
sriramraghavanm <117713430+sriramraghavanm@users.noreply.github.com>
2022-12-02 01:12:45 +0530

    Delete tcs-nqt-coding-sheet-tcs-coding-questions/Problems_on_Numbers directory

--- tcs-nqt-coding-sheet-tcs-coding-questions/Problems_on_Numbers/Program34.py
+++
@@ -1,9 +0,0 @@
-# Calculate the Area of the Circle
-# Problem Statement: Given the radius of the circle, calculate the area of the circle.
-
-radius = int(input("Enter the radius of the circle - "))
-pi = 3.14
-
-# Area of the Circle is πr2
-area = float(pi * radius * radius)
-print("Area of the circle with radius ",radius," is ", area)
--- tcs-nqt-coding-sheet-tcs-coding-questions/Problems_on_Numbers/Program7.py
+++
@@ -1,8 +0,0 @@
-# Check whether a given number is even or odd
-# Problem Statement: Given a number n, check whether a given number is even or odd.
-
-no = input("Enter a number - ")
-if int(no) % 2 == 0:
-    print("Number is even.")
-else:
-    print("Number is odd.")
