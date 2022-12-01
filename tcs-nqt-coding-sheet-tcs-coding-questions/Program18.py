16c1a35f647238374b187b060c6c39ab4d18eeb3
sriramraghavanm <raghavan.23@live.in>
2022-12-01 23:59:04 +0530

    Factorial of a Number

---
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program18.py
@@ -0,0 +1,9 @@
+# Factorial of a Number : Iterative and Recursive
+# Problem Statement: Given a number X,  print its factorial.
+
+no = int(input("Enter a number - "))
+factorial = 0
+
+
+
+print("Factorial of the number ",no," is ",factorial)
