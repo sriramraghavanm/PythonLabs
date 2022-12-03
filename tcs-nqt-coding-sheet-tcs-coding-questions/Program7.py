1aae08d41cecd5d304a6cf5a37f18f28cd23e68e
sriramraghavanm <raghavan.23@live.in>
2022-12-04 01:38:51 +0530

    made some changes to code

--- tcs-nqt-coding-sheet-tcs-coding-questions/Program7.py
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program7.py
@@ -1,8 +1,19 @@
 # Check whether a given number is even or odd
 # Problem Statement: Given a number n, check whether a given number is even or odd.
 
-no = input("Enter a number - ")
-if int(no) % 2 == 0:
+# Example 1:
+# Input: n=5
+# Output: odd
+# Explanation: 5 is not divisible by 2.
+ 
+# Example 2:  
+# Input: n=6
+# Output: even
+# Explanation: 6 is divisible by 2.
+
+no = int(input("Enter a number "))
+
+if no % 2 == 0:
     print("Number is even.")
 else:
     print("Number is odd.")
