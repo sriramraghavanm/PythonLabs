a545de84e8edd3bcd39ad59cc6d6c41237790113
sriramraghavanm <raghavan.23@live.in>
2022-12-04 01:48:45 +0530

    made some changes to code

--- tcs-nqt-coding-sheet-tcs-coding-questions/Program8.py
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program8.py
@@ -1,9 +1,19 @@
 # Check whether a number is positive or negative
 # Problem statement: Given a number n check whether it’s positive or negative.
 
-no = input("Enter a number - ")
+# Example 1:
+# Input: n=5
+# Output: Positive
 
-if int(no) >= 0:
+# Example2:
+# Input: n=-6
+# Output: Negative
+
+no = int(input("Enter a number - "))
+
+if no == 0:
+    print("The number zero is neither positive nor negative.")
+elif no > 0:
     print("Number is positive.")
 else:
     print("Number is negative.")
