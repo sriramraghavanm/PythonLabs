f7dcd5ccde68ceb52551304d4e30d0a015cbc94e
sriramraghavanm <raghavan.23@live.in>
2022-12-04 01:16:57 +0530

    added other use cases

--- tcs-nqt-coding-sheet-tcs-coding-questions/Program12.py
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program12.py
@@ -1,10 +1,19 @@
 # Greatest of two numbers
 # Problem Statement: Given two numbers. Find the greatest of two numbers.
 
+# Example 1:
+# Input: 1 3
+# Output: 3
+# Explanation: Answer is 3,since 3 is greater than 1.
+
 no1 = int(input("Enter number1 - "))
 no2 = int(input("Enter number2 - "))
 
-if no1 > no2:
+if (no1 == 0) and (no2 == 0):
+    print("Both the numbers are 0")
+elif no1 == no2:
+    print("Both the numbers are same")
+elif no1 > no2:
     print(no1, " is greater than ", no2)
 else:
     print(no2, " is greater than ", no1)
