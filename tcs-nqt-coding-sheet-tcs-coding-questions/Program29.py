ec2f2aacd45e6aebaa92a705964bb2e4aa14fa85
sriramraghavanm <raghavan.23@live.in>
2022-12-04 11:24:46 +0530

    added other use cases

--- tcs-nqt-coding-sheet-tcs-coding-questions/Program29.py
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program29.py
@@ -1,29 +1,35 @@
 # Find the sum of numbers in the given range
 # Problem Statement: Find the sum of numbers in the given range.
 
+# Example 1:
+# Input: l=2, r=7
+# Output: 27
+# Explanation: 2+3+4+5+6+7=27. Therefore 27 is the answer.
+
+# Example 2:
+# Input: l=5, r=9
+# Output: 35
+# Explanation: 5+6+7+8+9=35. Therefore 35 is the answer.
+
 startNo = int(input("Enter starting number - "))
 endNo = int(input("Enter ending number - "))
 
 sum = 0
-temp = startNo
 
-print("Starting number is ", startNo)
-
-while temp <= endNo:
-    sum = sum + temp
-    temp = temp + 1
-    if temp < endNo:
-        print(temp)
-
-print("Ending number is ", endNo)
-
-print("Using While Loop: Sum of all the numbers between ", startNo, " and ", endNo, " is ", sum)
-
-# Approach 2 - Using For Loop
-sum = 0
-
-for i in range(endNo + 1):
-    if i >= startNo:
-        print(i)
-        sum = sum + i
-print("Using For Loop: Sum of all the numbers between ", startNo, " and ", endNo, " is ", sum)
+if startNo == endNo:
+    print("startNo and endNo cannot be same")
+elif startNo > endNo:
+    print("startNo cannot be greater than endNo")
+else:
+    # Approach 1 - Using For Loop
+    for no in range(startNo,endNo + 1):
+        sum = sum + no
+    print("Using For Loop: Sum of all the numbers between ", startNo, " and ", endNo, " is ", sum)
+    
+    # Approach 2 - Using While Loop
+    sum = 0
+    temp = startNo
+    while temp < endNo + 1:
+        sum = sum + temp
+        temp = temp + 1
+    print("Using While Loop: Sum of all the numbers between ", startNo, " and ", endNo, " is ", sum)
