a4a489c1ce9f6cdca11f7578247aed80e9599745
sriramraghavanm <raghavan.23@live.in>
2022-12-05 23:43:54 +0530

    added check for less than equal to 0

--- tcs-nqt-coding-sheet-tcs-coding-questions/Program14.py
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program14.py
@@ -13,8 +13,8 @@
 
 year = int(input("Enter a year "))
 
-if year == 0:
-    print("Year cannot be 0")
+if year <= 0:
+    print("Year has to be greater than 0!!!")
 elif year % 4 == 0 and year % 100 != 0:
     print(year," is a leap year")
 elif year % 400 == 0:
