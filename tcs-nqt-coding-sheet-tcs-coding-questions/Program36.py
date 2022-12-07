0a3f9f8035b8347a7dd3a7b41646258d569dfac3
sriramraghavanm <raghavan.23@live.in>
2022-12-07 23:42:10 +0530

    swap two numbers

---
+++ tcs-nqt-coding-sheet-tcs-coding-questions/Program36.py
@@ -0,0 +1,17 @@
+# Program to swap two numbers
+# Example a = 10, b = 5; output should be a = 5 and b = 10
+
+no1 = int(input("Enter no1 "))
+no2 = int(input("Enter no2 "))
+
+# Approach 1: Swapping 2 numbers using a temp variable
+
+print("Before swapping no1 = ",no1," and no2 = ",no2)
+
+if no1 == no2:
+    print("Both the numbers are same")
+else:
+    temp = no1
+    no1 = no2
+    no2 = temp
+    print("After swapping no1 = ",no1," and no2 = ",no2)
