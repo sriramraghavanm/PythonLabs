7c7522c1daa97d1afcf504ec2b7860231362c3d6
sriramraghavanm <117713430+sriramraghavanm@users.noreply.github.com>
2022-12-16 23:45:17 +0530

    Update python_operators.py

--- Python-Class/python_operators.py
+++ Python-Class/python_operators.py
@@ -18,3 +18,53 @@ print("Float Division of x / y = ", x / y)
 print("Integer Division of x // y = ", x // y)
 print("Modulus of x % y = ", x % y)
 print("Power of y on x i.e. x ** y = ", x ** y)
+
+# some operations on string
+str_data = "raghavan"
+empty_str = ''
+
+# concat operation for string
+full_name = str_data + " " + "mohan"
+print("Full name = ", full_name)
+
+# Can we use '-' as well? It will not work.
+# minus_str = "raghavan" - "mohan"
+# print("Minus str = ", minus_str)
+
+# multiply_str = "raghavan" * "mohan"
+# print("Multiply str = ", multiply_str)
+
+# power_str = "raghavan" ** "mohan"
+# print("Power str = ", power_str)
+
+# power_str = "raghavan" ** 3
+# print("Power str = ", power_str)
+
+multiply_numeric_str = "raghavan" * 3
+print("Multiply numeric str = ", multiply_numeric_str)
+
+# Assignment operators
+# =, x = 5
+# +=, x += 5 -> x = x + 5
+# -=, x -= 5 -> x = x - 5
+# *=, x *= 5 -> x = x * 5
+# /=, x /= 5 -> x = x / 5
+# //=, x //= 5 -> x = x // 5
+# %=, x %= 5 -> x = x % 5
+
+# Comparision operators
+# == , Equals to condition, x == y
+# != , Not equals to condition, x != y
+# >  , Greater than condition, x > y
+# <  , Less than condition, x < y
+# >= , Greater than and equals to condition, x >= y
+# <= , Less than and equals to condition, x <= y
+
+a = 10
+b = 5
+print("Result of a == b", a == b)
+print("Result of a != b", a != b)
+print("Result of a > b", a > b)
+print("Result of a < b", a < b)
+print("Result of a >= b", a >= b)
+print("Result of a <= b", a <= b)
