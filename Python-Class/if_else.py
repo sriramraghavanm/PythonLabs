b02f23131aa4bb3ade07c3c145673a981329159d
sriramraghavanm <117713430+sriramraghavanm@users.noreply.github.com>
2022-12-17 15:51:40 +0530

    Update if_else.py

--- Python-Class/if_else.py
+++ Python-Class/if_else.py
@@ -19,3 +19,18 @@ a = 40
 if a == 50:
     print("Yes, a is equal to 50!!!")
 print("Bye")
+
+# Nested if-else condition
+
+marks = 88
+
+if marks > 90:
+    print("Grade A+")
+elif marks >= 80 and marks < 90:
+    print("Grade A")
+elif marks >= 70 and marks < 80:
+    print("Grade B+")
+elif marks >= 60 and marks < 70:
+    print("Grade B")
+else:
+    print("Grade C")
