a557443035777b25cd7a2729fcd37e231044101e
sriramraghavanm <117713430+sriramraghavanm@users.noreply.github.com>
2022-12-22 14:54:51 +0530

    Create tuples.py

---
+++ Python-Class/tuples.py
@@ -0,0 +1,19 @@
+# Working with Tuple - Immutable object similar to list
+
+# empty tuple
+t1 = ()
+
+# failed assignment and append
+# t1.append(3)
+# t1[0] = 3
+
+t2 = (20,30,40,50)
+print(len(t2))
+print(t2)
+
+# Indexing also works for tuple i.e. we can access the elements using their index
+# Accessing individual elements from the tuple
+print(t2[0])
+print(t2[1])
+print(t2[2])
+print(t2[3])
